14a3c1b5cb6a2c285787c9e74bf1c8e70c8ca906
masterUNG <phrombutr@gmail.com>
2018-10-06 15:45:16 +0700

    Change Label and Button

--- HelloUnG/ViewController.swift
+++ HelloUnG/ViewController.swift
@@ -9,7 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let nameString: String = "Thailand"
+    let currentNameString: String = "Master UNG"
+    
+    let titleTrueButtonString: String = "Change Label"
+    let titlwFalseButtonString: String = "Undo"
+    
+    
+    
+    var statusBool: Bool = true
+    
+    
+    
+    
+    @IBOutlet weak var titleButton: UIButton!
+    
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    
+    @IBAction func changeButton(_ sender: Any) {
+        
+        
+        statusBool = !statusBool
+        
+        if statusBool {
+            titleLabel.text = currentNameString
+            titleButton.setTitle(titleTrueButtonString, for: .normal)
+        }else{
+            titleButton.setTitle(titlwFalseButtonString, for: .normal)
+            titleLabel.text = nameString
+        }
+        
+        
+    }   // changeButton
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
